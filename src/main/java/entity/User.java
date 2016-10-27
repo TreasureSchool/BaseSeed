55dda35a1e7abb86ed2184ee0c9cdc2a85236fb9
Joachim <Joachim@Joachim-Pc-2014.home>
2016-10-27 16:38:53 +0200

    heres the base lets see if i can't finish it later for now it's good

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,51 +1,86 @@
 package entity;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import security.IUser;
 
-public class User implements IUser{
-  
-  private String password;  //Pleeeeease dont store me in plain text
-  private String userName;
-  List<String> roles = new ArrayList();
+@Entity
+public class User implements IUser, Serializable {
 
-  public User(String userName, String password) {
-    this.userName = userName;
-    this.password = password;
-  }
-  
-  public User(String userName, String password,List<String> roles) {
-    this.userName = userName;
-    this.password = password;
-    this.roles = roles;
-  }
-  
-  public void addRole(String role){
-    roles.add(role);
-  }
-    
-  @Override
-  public List<String> getRolesAsStrings() {
-   return roles;
-  }
- 
-  public String getPassword() {
-    return password;
-  }
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String password;  //Pleeeeease dont store me in plain text
+    private String userName;
+    List<String> roles = new ArrayList();
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 
-  public String getUserName() {
-    return userName;
-  }
+    public User(String userName, String password, List<String> roles) {
+        this.userName = userName;
+        this.password = password;
+        this.roles = roles;
+    }
 
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
+    public void addRole(String role) {
+        roles.add(role);
+    }
 
- 
-          
+    @Override
+    public List<String> getRolesAsStrings() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.User[ id=" + id + " ]";
+    }
+
 }
--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -10,7 +10,7 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.view3',
-  'myApp.view4',
+  'myApp.view5',
   'myApp.filters',
   'myApp.directives',
   'myApp.factories',
